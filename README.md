2757f8ade66184f890cb7e10b956869cc11b3aaf
nihatbayramm <116274930+nihatbayramm@users.noreply.github.com>
2024-10-31 13:01:57 +0300

    first commit

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Bootsrap
 # Bootsrap
 # bootstrap
+# JavaScript
---
+++ java script/degiskenTurleri.js
@@ -0,0 +1,6 @@
+//Değişken türleri
+
+var firstName="NİHAT"; //String değer tipi 
+var  years = 1903;  // integer değer tipi
+
+console.log(typeof firstName);
